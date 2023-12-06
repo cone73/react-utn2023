68def96004d9f1cd3944b87efdbb852b053a4a4d
cone2023 <profeheredia66@gmail.com>
2023-12-05 21:41:09 -0300

    componentes 3

---
+++ src/componentes/Producto.jsx
@@ -0,0 +1,34 @@
+import './Producto.css';
+
+export default function Producto () {
+    const imagen = "imagenes/potter_lego.webp";
+    const titulo = "Lego Harry Potter";
+    const precio_anterior = 45000;
+    const precio_actual = 35000;
+    const envio_gratis = true;
+    // - Botón de "Comprar": HTML_Element
+
+    return (
+        <div className='producto'>
+            <img src={imagen} alt="" />
+            <div className='producto-info'>
+                <h2>{titulo}</h2>
+                <p>
+                    <del className='precio-anterior'>
+                        ${precio_anterior}
+                    </del>
+                    <br />
+                    <span className='precio-actual'>
+                        ${precio_actual}
+                    </span>
+                </p>
+                {/* Si envio_gratis es true ENTONCES && */}
+                {envio_gratis &&
+                    <p className='envio-gratis'>
+                        Envío gratis full
+                    </p>
+                }
+            </div>
+        </div>
+    )
+}
